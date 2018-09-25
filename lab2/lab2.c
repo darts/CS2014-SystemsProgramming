d17fbcbce7ec88aa70213f00c69d8141b8d45e1a
dartse <darts@tcd.ie>
2018-09-25 20:09:57 +0100

    Finished lab2
    Finished lab2 part 1, works and is commented thoroughly.

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include <math.h>
 
 struct listNode //a node on the list
 {
-    double data;              //data in the node
+    double data;           //data in the node
     struct listNode *next; //pointer to next node
 };
 
@@ -29,68 +30,79 @@ void add_to_front_of_list(struct list *this, int data)
     this->head = node;                                       // set the listNode pointer to be the new node
 }
 
-int getPrev(struct list *list){
-    int numRet = (list->head)->data;
-    struct listNode *head = list->head;
-    list->head = (list->head)->next;
-    free(head);
-    return numRet;
+double getPrev(struct list *list)
+{
+    double numRet = (list->head)->data;      //get the data from the node
+    struct listNode *tmpHead = (list->head); //get a temporary pointer to the node
+    list->head = (list->head)->next; //update the pointer in head to point to the next node
+    free(tmpHead);  //free the old node
+    return numRet;  //return the data
 }
 
-void multiply(struct list *list){
-    double numA = getPrev(list);
-    double numB = getPrev(list);
+void multiply(struct list *list)
+{
+    double numA = getPrev(list); //get the previous number
+    double numB = getPrev(list); //^^
 
-    numA = numA * numB;
-    add_to_front_of_list(list, numA);
+    numA = numA * numB; //multiply the two
+    add_to_front_of_list(list, numA); //add to the front of the list
 }
 
-void sum(struct list *list){
-    double numA = getPrev(list);
-    double numB = getPrev(list);
+void sum(struct list *list)
+{
+    double numA = getPrev(list); //get the previous number
+    double numB = getPrev(list); //^^
 
-    numA = numA * numB;
-    add_to_front_of_list(list, numA);
+    numA = numA + numB; //sum the two
+    add_to_front_of_list(list, numA); //add to the front of the list
 }
 
-void subtract(struct list *list){
-    double numA = getPrev(list);
-    double numB = getPrev(list);
+void subtract(struct list *list)
+{
+    double numA = getPrev(list); //get previous number
+    double numB = getPrev(list); //^^
 
-    numA = numB - numA;
-    add_to_front_of_list(list, numA);
+    numA = numB - numA; //get the difference
+    add_to_front_of_list(list, numA); //add to the front of the list
 }
 
-void div(struct list *list){
-    double numA = getPrev(list);
-    double numB = getPrev(list);
+void divide(struct list *list)
+{
+    double numA = getPrev(list); //get previous number 
+    double numB = getPrev(list); //^^
 
-    numA = numB / numA;
-    add_to_front_of_list(list, numA);
+    numA = numB / numA; //divide the numbers
+    add_to_front_of_list(list, numA); //add to the front of the list
 }
 
-void pow(struct list *list){
-    double numA = getPrev(list);
-    double numB = getPrev(list);
+void power(struct list *list)
+{
+    double numA = getPrev(list);//get the previous number 
+    double numB = getPrev(list);//^^
 
-    numA = numB ^ numA;
-    add_to_front_of_list(list, numA);
+    numA = pow(numA, numB); //raise to the power of....
+    add_to_front_of_list(list, numA); //add to the front of the list
+}
+
+double normalise(double prev, double current) //for multiple-digit numbers
+{
+    prev = prev * 10; //offset by x10
+    prev += current; //add to prev
+    return prev; //return 
 }
 
 int main(int argc, char const *argv[])
 {
-    //     //struct list *newList = new_empty_list();
-    //     //add_to_front_of_list(newList, 12);
-
     double itemRead;
     FILE *file;
     file = fopen("input.txt", "r");
     if (file)
     {
-        struct list *numList = new_empty_list();
-        while ((itemRead = getc(file)) != EOF)
-        {
-            putchar(itemRead);
+        double prev = -1; //no prev number
+        struct list *numList = new_empty_list(); //new list
+        while ((itemRead = getc(file)) != EOF) //while there are still numbers
+        {   
+            putchar(itemRead); //show the character being read in
             if (itemRead == 'X')
             {
                 multiply(numList);
@@ -101,20 +113,36 @@ int main(int argc, char const *argv[])
             }
             else if (itemRead == '/')
             {
-                div(numList);
+                divide(numList);
             }
             else if (itemRead == '^')
             {
-                pow(numList);
+                power(numList);
+            }
+            else if (itemRead == '+')
+            {
+                sum(numList);
             }
             else
             {
-                int num = itemRead;
-                add_to_front_of_list(numList, num);
+                if (itemRead == ' ')
+                {
+                    if(prev != -1){ //has previous number
+                        add_to_front_of_list(numList, prev);
+                        prev = -1; //reset to false
+                    }
+                }
+                else
+                {
+                    double num = itemRead - '0'; //convert to double
+                    if(prev == -1)//if this is the first number, set prev to 0
+                        prev = 0;
+                    prev = normalise(prev, num);
+                }
             }
         }
         fclose(file);
-        printf("%f \n", numList->head->data);
+        printf("\nThe result is:%f \n", (numList->head)->data);
     }
     return 0;
 }
