3782ec134006c71d739fe50d69d3406d35a4214c
dartse <darts@tcd.ie>
2018-09-26 18:31:21 +0100

    Actually finished lab2 p1
    Now does infix notation which I forgot to do but sure look. It works now.

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -1,3 +1,6 @@
+//Infix-Notated inputs must be wrapped in brackets eg 3 + 5 -> ( 3 + 5 )
+//Must have space between each number/operator/bracket
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
@@ -14,7 +17,7 @@ struct list //pointer to a node
     struct listNode *head; //create a pointer called head to point at first node in list
 };
 
-struct list *new_empty_list() //creates a pointer to a list
+struct list *newEmptyList() //creates a pointer to a list
 {
     struct list *result;                  //creates a pointer to a list
     result = malloc(sizeof(struct list)); //allocate memory to the list
@@ -22,7 +25,7 @@ struct list *new_empty_list() //creates a pointer to a list
     return result;                        // return the pointer to the list
 };
 
-void add_to_front_of_list(struct list *this, int data)
+void addToFrontOfList(struct list *this, double data) //Add to the front of the list/top of the stack
 {
     struct listNode *node = malloc(sizeof(struct listNode)); // allocate memory to new listnode
     node->data = data;                                       //set the data in the new node to be the data that was passed
@@ -30,7 +33,27 @@ void add_to_front_of_list(struct list *this, int data)
     this->head = node;                                       // set the listNode pointer to be the new node
 }
 
-double getPrev(struct list *list)
+void addToEndOfList(struct list *list, double data) //Add to the end of the list/bottom of the stack
+{
+    struct listNode *node = malloc(sizeof(struct listNode)); // allocate memory to new listnode
+    struct listNode *tmpHead = list->head; //create new temporary node
+    if (list->head != NULL)   //if the list size is not 0
+    {
+        while (tmpHead->next != NULL)  //while not at the end of the list
+        {
+            tmpHead = tmpHead->next;   //Iterate through the list
+        }
+        tmpHead->next = node;  //point to new node
+    }
+    else
+    {
+        list->head = node; //point to new node
+    }
+    node->data = data;  //add new data to the node
+    node->next = NULL;  //give the node a new pointer
+}
+
+double getPrev(struct list *list)   //get the previous node and remove it
 {
     double numRet = (list->head)->data;      //get the data from the node
     struct listNode *tmpHead = (list->head); //get a temporary pointer to the node
@@ -44,8 +67,8 @@ void multiply(struct list *list)
     double numA = getPrev(list); //get the previous number
     double numB = getPrev(list); //^^
 
-    numA = numA * numB;               //multiply the two
-    add_to_front_of_list(list, numA); //add to the front of the list
+    numA = numA * numB;           //multiply the two
+    addToFrontOfList(list, numA); //add to the front of the list
 }
 
 void sum(struct list *list)
@@ -53,8 +76,8 @@ void sum(struct list *list)
     double numA = getPrev(list); //get the previous number
     double numB = getPrev(list); //^^
 
-    numA = numA + numB;               //sum the two
-    add_to_front_of_list(list, numA); //add to the front of the list
+    numA = numA + numB;           //sum the two
+    addToFrontOfList(list, numA); //add to the front of the list
 }
 
 void subtract(struct list *list)
@@ -62,8 +85,8 @@ void subtract(struct list *list)
     double numA = getPrev(list); //get previous number
     double numB = getPrev(list); //^^
 
-    numA = numB - numA;               //get the difference
-    add_to_front_of_list(list, numA); //add to the front of the list
+    numA = numB - numA;           //get the difference
+    addToFrontOfList(list, numA); //add to the front of the list
 }
 
 void divide(struct list *list)
@@ -71,8 +94,8 @@ void divide(struct list *list)
     double numA = getPrev(list); //get previous number
     double numB = getPrev(list); //^^
 
-    numA = numB / numA;               //divide the numbers
-    add_to_front_of_list(list, numA); //add to the front of the list
+    numA = numB / numA;           //divide the numbers
+    addToFrontOfList(list, numA); //add to the front of the list
 }
 
 void power(struct list *list)
@@ -80,15 +103,14 @@ void power(struct list *list)
     double numA = getPrev(list); //get the previous number
     double numB = getPrev(list); //^^
 
-    numA = pow(numA, numB);           //raise to the power of....
-    add_to_front_of_list(list, numA); //add to the front of the list
+    numA = pow(numB, numA);       //raise to the power of....
+    addToFrontOfList(list, numA); //add to the front of the list
 }
 
 void normalise(double *prev, double current) //for multiple-digit numbers
 {
     *prev = *prev * 10; //offset by x10
-    *prev += current;  //add to prev
-   // return *prev;      //return
+    *prev += current;   //add to prev
 }
 
 void updatePostFix(double itemRead, struct list *numList, double *prev)
@@ -117,32 +139,120 @@ void updatePostFix(double itemRead, struct list *numList, double *prev)
     {
         if (itemRead == ' ')
         {
-            if (*prev != -1)
-            { //has previous number
-                add_to_front_of_list(numList, *prev);
+            if (*prev != -1)//has previous number
+            { 
+                addToFrontOfList(numList, *prev);
                 *prev = -1; //reset to false
             }
         }
         else
         {
             double num = itemRead - '0'; //convert to double
-            if (*prev == -1)              //if this is the first number, set prev to 0
+            if (*prev == -1)             //if this is the first number, set prev to 0
                 *prev = 0;
-            normalise(prev, num);
+            normalise(prev, num);   //convert to a proper int ie 1 5 -> 15
         }
     }
-    //return prev;
 }
 
-void evalInput(int *isInfix, double itemRead, struct list *numList, double *prev, int *firstCharIsBracket){
-    if(*firstCharIsBracket == -1){
-        if(itemRead == '('){
-            *firstCharIsBracket = 1;
-        }else{
-            *firstCharIsBracket = 0;
+void manageInFixStack(double itemRead, double *prev, struct list *opList, struct list *rpnList)
+{
+    if (itemRead == '(')
+    {
+        addToFrontOfList(opList, itemRead);
+    }
+    else if (itemRead == ')')
+    {
+        while (opList->head->data != '(')
+        {
+            addToEndOfList(rpnList, getPrev(opList));
+            addToEndOfList(rpnList, ' ');
         }
-    }else{ 
-        
+        getPrev(opList);
+    }
+    else if (itemRead == '^')//check precedence
+    {
+        while (opList->head->data == '^')//check precedence
+        {
+            addToEndOfList(rpnList, getPrev(opList));
+            addToEndOfList(rpnList, ' ');
+        }
+        addToFrontOfList(opList, itemRead);
+    }
+    else if (itemRead == 'X' || itemRead == '/')//check precedence
+    {
+        while (opList->head->data == '^')//check precedence
+        {
+            addToEndOfList(rpnList, getPrev(opList));
+            addToEndOfList(rpnList, ' ');
+        }
+        addToFrontOfList(opList, itemRead);
+    }
+    else if (itemRead == '-' || itemRead == '+')//check precedence
+    {
+        while (opList->head->data == '^' || opList->head->data == 'X' || opList->head->data == '/')//check precedence
+        {
+            addToEndOfList(rpnList, getPrev(opList));
+            addToEndOfList(rpnList, ' ');
+        }
+        addToFrontOfList(opList, itemRead);
+    }
+    else
+    {
+        if (itemRead == ' ')
+        {
+            if (*prev != -1) //has previous number
+            {
+                addToEndOfList(rpnList, ' ');
+                *prev = -1; //reset to false
+            }
+        }
+        else
+        {
+            double num = itemRead - '0'; //convert to double
+            addToEndOfList(rpnList, itemRead);
+            if (*prev == -1) //if this is the first number, set prev to 0
+                *prev = 0;
+        }
+    }
+}
+
+//evaluate the input (RPN / infix)
+void evalInput(int *firstCharIsBracket, double itemRead, double *prev, 
+               struct list *numList, struct list *opList, struct list *rpnList)
+{
+    if (*firstCharIsBracket == -1)//first char
+    {
+        if (itemRead == '(')
+        {
+            *firstCharIsBracket = 1;//use infix
+            manageInFixStack(itemRead, prev, opList, rpnList);
+        }
+        else
+        {
+            *firstCharIsBracket = 0;
+            updatePostFix(itemRead, numList, prev);
+        }
+    }
+    else if (*firstCharIsBracket == 0)//use RPN
+    {
+        updatePostFix(itemRead, numList, prev);
+    }
+    else
+    {
+        manageInFixStack(itemRead, prev, opList, rpnList);//use infix
+    }
+}
+
+//infix has been converted to RPN, now calculate
+void postfixOnStack(struct list *rpnList, struct list *numList) 
+{
+    double prevNum = -1;    
+    double *prev = &prevNum;
+    while (rpnList->head != NULL)
+    {
+        updatePostFix(rpnList->head->data, numList, prev); //use RPN calc
+        getPrev(rpnList); //remove whitespace
     }
 }
 
@@ -155,15 +265,38 @@ int main(int argc, char const *argv[])
     {
         int firstCharIsBracket = -1;
         int *firstCharIsBracketBool = &firstCharIsBracket;
-        int infix = 0;
-        int *isInfix = &infix;
         double prevNum = -1;
-        double *prev = &prevNum;                        //no prev number
-        struct list *numList = new_empty_list(); //new list
-        while ((itemRead = getc(file)) != EOF)   //while there are still numbers
+        double *prev = &prevNum;               //no prev number
+        struct list *numList = newEmptyList(); //new list
+        struct list *opList = newEmptyList();  //new tmp list for infix -> RPN
+        struct list *rpnList = newEmptyList(); //output list
+        while ((itemRead = getc(file)) != EOF) //while there are still numbers
         {
-            putchar(itemRead); //show the character being read in
-            updatePostFix(itemRead,numList,prev);
+            if (itemRead != '\n')
+            {
+                putchar(itemRead); //show the character being read in
+                evalInput(firstCharIsBracketBool, itemRead, prev, numList, opList, rpnList);
+            }
+            else
+            {
+                printf("\n");
+                if (firstCharIsBracket == 1)
+                {
+                    firstCharIsBracket = 0;
+                    postfixOnStack(rpnList, numList);
+                }
+                printf("\nThe result is:%f \n\n", (numList->head)->data);
+                prevNum = -1;
+                firstCharIsBracket = -1;
+                numList = newEmptyList();
+                opList = newEmptyList();
+                rpnList = newEmptyList();
+            }
+        }
+        if (firstCharIsBracket == 1)
+        {
+            firstCharIsBracket = 0;
+            postfixOnStack(rpnList, numList);
         }
         fclose(file);
         printf("\nThe result is:%f \n", (numList->head)->data);
