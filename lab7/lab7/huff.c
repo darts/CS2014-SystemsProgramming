0a24974e33feeb43e06a1059bd20c47f28a149ae
dartse <darts@tcd.ie>
2018-11-23 15:11:23 +0000

    Formatting changes

--- lab7/lab7/huff.c
+++ lab7/lab7/huff.c
@@ -31,8 +31,9 @@ void huffcoder_count(struct huffcoder *this, char *filename)
     this->freqs[c]++; //increment the counter of the character found
     c = fgetc(file);  //get the next character
   }
-  for(int i = 0; i < NUM_CHARS; i++){
-    if(this->freqs[i] == 0)
+  for (int i = 0; i < NUM_CHARS; i++)
+  {
+    if (this->freqs[i] == 0)
       this->freqs[i] = 1;
   }
   fclose(file);
@@ -102,7 +103,7 @@ void huffcoder_build_tree(struct huffcoder *this)
     charArr[i] = theChar;
   }
   //Combine the two smallest huffchars until one is left
-  for (int i = 0; i < NUM_CHARS -1; i++)
+  for (int i = 0; i < NUM_CHARS - 1; i++)
   {
     sortList(charArr, NUM_CHARS - i);
     charArr[0] = combine_huffs(charArr[0], charArr[1], NUM_CHARS + i);
@@ -128,9 +129,10 @@ void tree2table_recursive(struct huffcoder *this, struct huffchar *node,
   else
   {
     //My code was printing mirror images of the desired result (eg 001001 -> 100100).
-    //This "fixes" it. 
+    //This "fixes" it.
     int newPath = 0;
-    for(int i = 0; i <= depth; i++){
+    for (int i = 0; i <= depth; i++)
+    {
       newPath |= (path >> i) & 1;
       newPath = newPath << 1;
     }
@@ -171,22 +173,26 @@ void huffcoder_print_codes(struct huffcoder *this)
     ;
   }
 }
-void printBin(unsigned num){
-    unsigned mask = 2147483648;
-    for(int i = 0; i < 32; i++){
-        if((num & mask) == 0)
-            fprintf(stderr,"0");
-        else    
-            fprintf(stderr,"1");
-        num<<=1;
-    }
-    fprintf(stderr,"\n");
+void printBin(unsigned num)
+{
+  unsigned mask = 2147483648;
+  for (int i = 0; i < 32; i++)
+  {
+    if ((num & mask) == 0)
+      fprintf(stderr, "0");
+    else
+      fprintf(stderr, "1");
+    num <<= 1;
+  }
+  fprintf(stderr, "\n");
 }
 
-void writeEOT(struct huffcoder * this, struct bitfile * bitF){
+void writeEOT(struct huffcoder *this, struct bitfile *bitF)
+{
   int EOTlength = this->code_lengths[EOTloc];
   int EOTcode = this->codes[EOTloc];
-  for(int i = 0; i < EOTlength; i++){
+  for (int i = 0; i < EOTlength; i++)
+  {
     bitfile_write_bit(bitF, (EOTcode >> i) & 1);
   }
 }
@@ -195,13 +201,15 @@ void writeEOT(struct huffcoder * this, struct bitfile * bitF){
 void huffcoder_encode(struct huffcoder *this, char *input_filename,
                       char *output_filename)
 {
-  struct bitfile * outFile = bitfile_open(output_filename, "w");
-  FILE * readFile = fopen(input_filename, "r");
+  struct bitfile *outFile = bitfile_open(output_filename, "w");
+  FILE *readFile = fopen(input_filename, "r");
   unsigned char currentChar = fgetc(readFile);
-  while(!feof(readFile)){
+  while (!feof(readFile))
+  {
     int numToWrite = this->codes[currentChar];
     int max = this->code_lengths[currentChar];
-    for(int i = 0; i < max; i++){
+    for (int i = 0; i < max; i++)
+    {
       bitfile_write_bit(outFile, (numToWrite >> i) & 1);
     }
     currentChar = fgetc(readFile);
@@ -210,8 +218,9 @@ void huffcoder_encode(struct huffcoder *this, char *input_filename,
   bitfile_close(outFile);
 }
 
-struct huffchar * walk_tree(struct huffchar * this, int bit){
-  if(bit == 0)
+struct huffchar *walk_tree(struct huffchar *this, int bit)
+{
+  if (bit == 0)
     return this->u.compound.left;
   return this->u.compound.right;
 }
@@ -220,22 +229,24 @@ struct huffchar * walk_tree(struct huffchar * this, int bit){
 void huffcoder_decode(struct huffcoder *this, char *input_filename,
                       char *output_filename)
 {
-  struct bitfile * rFile = bitfile_open(input_filename, "r");
- FILE * outFile = fopen(output_filename,"r");
+  struct bitfile *rFile = bitfile_open(input_filename, "r");
+  FILE *outFile = fopen(output_filename, "r");
 
   unsigned char theChar;
   int outChar = 0;
   int finished = 0;
-  struct huffchar * currentHuff = this->tree;
-  while(!finished){
+  struct huffchar *currentHuff = this->tree;
+  while (!finished)
+  {
     int theBit = bitfile_read_bit(rFile);
     currentHuff = walk_tree(currentHuff, theBit);
-    if(!currentHuff->is_compound){
-      if(currentHuff->u.c != EOTloc)
-        fputc(currentHuff->u.c,outFile);
+    if (!currentHuff->is_compound)
+    {
+      if (currentHuff->u.c != EOTloc)
+        fputc(currentHuff->u.c, outFile);
       else
         finished = 1;
       currentHuff = this->tree;
     }
-  } 
+  }
 }
